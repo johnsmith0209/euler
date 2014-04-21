d7ed96dbf441a8081d4347d7304a9be087fc06d9
johnsmith0209 <781136509@qq.com>
2014-04-21 16:57:07 +0800

    Largest prime factor, too slow, should use Pollard's rho algorithm

--- 3.c
+++ 3.c
@@ -2,8 +2,6 @@
 #define LARGE 600851475143  
 
 int isPrime(unsigned long long num){
-	printf("    isPrime :%lld\n", num);
-
 	unsigned long long mid = num / 2; 
 
 	for(unsigned long long i = 2;i < mid ; i++){
@@ -18,7 +16,6 @@ int main(){
 	unsigned long long i, mid, factor;
 	mid = LARGE / 2;
 	for(i = 2; i < mid; i++){
-		// printf("factor %lld\n", i);
 		if( LARGE % i != 0) continue;
 		if(isPrime( i )){
 			factor = i;
